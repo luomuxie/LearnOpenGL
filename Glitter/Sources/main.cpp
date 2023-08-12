05944354733614a0453a2404843f6976679a8226
luomuxie <835657341@qq.com>
2023-08-12 18:17:38 +0800

    add uniform test

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -1,12 +1,17 @@
 #include "hello_rect.h"
+#include "shaders_uniform.h"
 
 int main(int argc, char* argv[]) {
         
 /*    hello_triangle hw;
     hw.hello_triangleOpenGL(); */   
 
-    hello_rect hr;
-    hr.hello_rectOpenGL();
+    //hello_rect hr;
+    //hr.hello_rectOpenGL();
+
+    shaders_uniform su;
+    su.shaders_uniformOpenGL();
+
 
 }
 
